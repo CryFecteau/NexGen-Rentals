4f8b041ff57ae5d4d0d5bebb5ee40151ca786b5e
Cry <cry.fecteau@gmail.com>
2023-06-20 08:27:02 -0700

    File setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { PopularComponent } from './components/popular/popular.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    PopularComponent,
+    FeaturesComponent,
+    OffersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
